8eaef5e0f1437bedcae967d6503edc2754a7b86e
serg <sakhom16@gmail.com>
2017-03-15 17:15:05 +0200

    add bootstrap templates for html pages

---
+++ src/main/java/com/lardi/phonebook/controller/MainController.java
@@ -0,0 +1,43 @@
+package com.lardi.phonebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+@Controller
+// мапим наш REST на /myservice
+@RequestMapping(value = "/myservice")
+public class MainController {
+    // этот метод будет принимать время методом GET и на его основе
+    // отвечать клиенту
+    @RequestMapping(value= "/{time}", method = RequestMethod.GET)
+    @ResponseBody
+    public MyDataObject getMyData(@PathVariable long time) {
+        return new MyDataObject(Calendar.getInstance(), "Это ответ метода GET!");
+    }
+
+    // этот метод будет принимать Объект MyDataObject и отдавать его клиенту
+    // обычно метод PUT используют для сохранения либо для обновления объекта
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseBody
+    public MyDataObject putMyData(@RequestBody MyDataObject md) {
+        return md;
+    }
+
+    // этот метод будет методом POST отдавать объект MyDataObject
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public MyDataObject postMyData() {
+        return new MyDataObject(Calendar.getInstance(), "это ответ метода POST!");
+    }
+
+    // этот метод будет принимать время методом DELETE
+    // и на его основе можно удалит объект
+    @RequestMapping(value= "/{time}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public MyDataObject deleteMyData(@PathVariable long time) {
+        return new MyDataObject(Calendar.getInstance(), "Это ответ метода DELETE!");
+    }
+}
