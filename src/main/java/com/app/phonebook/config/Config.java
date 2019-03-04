3a9114201438081a306884df758070e7d6eaa7a4
sergeykh <sergey.khomenko@nurd.com>
2019-03-04 12:21:59 +0200

    add internationalization, config changes

--- src/main/java/com/app/phonebook/config/Config.java
+++ src/main/java/com/app/phonebook/config/Config.java
@@ -5,7 +5,9 @@ import com.app.phonebook.service.SecurityService;
 import com.app.phonebook.service.impl.SecurityServiceImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -19,6 +21,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
@@ -39,13 +43,6 @@ public class Config extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService createUserDetailsService() {
         return new UserDetailsServiceImpl();
-    }
-
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.ENGLISH);
-        return slr;
     }
 
     @Bean
